9d2d7087ea20ee06332ff9e57a34229a146d1172
Prashant-AK <ps.prashantsingh.98@gmail.com>
2021-07-16 00:13:42 +0530

    mongo db atlas done

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-require("./model/connection");
+// require("./model/connection");
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const mongoose = require("mongoose");
+//DB Config
+const db = require("./model/key").MongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Mongodb Connected`))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
---
+++ model/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://hotel:1234567890@cluster0.etrlx.mongodb.net/Hotel?retryWrites=true&w=majority",
+};
